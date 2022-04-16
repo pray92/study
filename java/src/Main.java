9f92e1326dd394c5543143acb28e0ece9ce6a764
Jisu Choi <redgem92@gmail.com>
2022-04-16 22:57:09 +0900

    FEAT(InnerClass) : modifier example

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,24 @@
 import ch7.InterfaceTest2;
 
+interface a {
+    default void func() {
+        System.out.println("a");
+    }
+}
+
+interface b {
+    default void func() {
+        System.out.println("b");
+    }
+}
+
+// 클래스 B가 모호해요!
+class B implements a, b{
+    @Override 
+    public void func() {
+
+    }
+}
 public class Main{
     public static void main(String[] args) {
         InterfaceTest2.execute();
---
+++ java/src/ch7/InnerEx3.java
@@ -0,0 +1,31 @@
+package ch7;
+
+public class InnerEx3 {
+    private int outerIv = 0;
+    static int outerCv = 0;
+
+    class InstanceInner {
+        int iiv = outerIv; // private 멤버 접근 가능
+        int iiv2 = outerCv;
+    }
+
+    static class StaticInner {
+        // 스태틱 클래스는 외부 클래스의 인스턴스 멤버에 접근 못해요
+        // int siv = outerIv;
+        static int scv = outerCv;
+    }
+
+    void myMethod() {
+        int lv = 0;
+        final int LV = 0; // 1.8부터 생략 가능해요
+
+        class LocalInner {
+            int liv = outerIv;
+            int liv2 = outerCv;
+            // 외부 클래스의 지역 변수는 final이 붙은 변수(상수)만 접근 가능해요
+            // 는 1.8부터 안 붙어도 가능해요
+            int liv3 = lv;      // 1.8 이전에선 안되요
+            int liv4 = LV;
+        }
+    }
+}
