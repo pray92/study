d39a0d9defce4429c985e2615011340f8b09d712
Jisu Choi <redgem92@gmail.com>
2022-05-17 21:52:19 +0900

    FEAT(ThreadEx8) : Example for priority

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,7 @@
-import ch13.ThreadEx7;
+import ch13.ThreadEx8;
 
 public class Main {
     public static void main(String[] args) {
-        ThreadEx7.execute();
+        ThreadEx8.execute();
     }
 }
